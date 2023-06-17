406c6fd6531510ee32ddf26ed7d3bda3f04efb2d
Valera Kamelkov <tahaburrent@gmail.com>
2023-06-17 22:35:28 +0200

    ~

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -3,12 +3,14 @@
     openModalBtn: document.querySelector('[data-ingredients-modal-open]'),
     closeModalBtn: document.querySelector('[data-ingredients-modal-close]'),
     modal: document.querySelector('[data-ingredients-modal]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    refs.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('modal-is-hidden');
   }
 })();
